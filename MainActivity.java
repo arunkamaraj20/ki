8c37ada302888e35b62a2c0086507b4fb20b07d5
tinyboyak <arunkamaraj@student.tce.edu>
2024-05-23 13:08:40 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.socialmediaapp;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private MessageAdapter adapter;
+    private SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        db = new MessageDatabaseHelper(this).getReadableDatabase();
+
+        Cursor cursor = db.query("messages", null, null, null, null, null, null);
+        adapter = new MessageAdapter(this, cursor);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) adapter.getItem(position);
+                String userId = cursor.getString(cursor.getColumnIndex("user_id"));
+                Intent intent = new Intent(MainActivity.this, MessageDetailActivity.class);
+                intent.putExtra("user_id", userId);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
